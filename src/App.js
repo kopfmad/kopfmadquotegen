f881810ffba06ef9aa73fb374c2f66ffa2bfd757
kopfmad <santana.agr@gmail.com>
2021-07-23 14:41:21 -0300

    Iniciando o repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="QuoteContainer">
+        <div>Quote Here</div>
+        <div>Quote Author Here</div>
+        <button>Click to get a new quote</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
